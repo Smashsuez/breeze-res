6623f8280152b867b40055ee1132292f021d416a
Smashsuez <mohamedahmedgameel@gmail.com>
2023-11-06 11:21:16 +0200

    .

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Icon from './Icon'
 import Image from 'next/image'
@@ -6,6 +6,7 @@ import Link from 'next/link'
 
 
 const Footer = () => {
+  const [team, setTeam] = useState(false)
   return (
     <div>
     <div className='flex flex-wrap items-center  w-[100vw] min-h-[100vh] px-4' >
@@ -25,10 +26,24 @@ const Footer = () => {
           
         </div>
       </div>
-      <div className='font-capriola text-lg w-[100vw] h-fit text-center flex items-center justify-center'>
+      <button onClick={()=>setTeam(true)} className='font-capriola text-lg w-[100vw] h-fit text-center flex items-center justify-center'>
         Powered By <p className='text-blue-600 inline-flex items-center font-bold'>&nbsp;Breeze &nbsp;<span className='inline-block w-[50px] h-[50px] border-2 border-blue-600 rounded-full overflow-hidden'><Image alt='' src="/imgs/Breeze.jpg" width={60} height={60}/></span></p>
-      </div>
-      
+      </button>
+      {team && 
+        <div className='bg-blue-900/50 fixed h-[100vh] w-[100vw] top-0 right-0 left-0 flex justify-center items-center'>
+          <div className="flex flex-col gap-3 relative bg-white p-3 pt-9 w-[90vw] md:w-[65vw] min-w-[250px] h-fit min-h-[450px] max-w-[400px] rounded-lg">
+            <span onClick={()=>setCash(false)} className="w-6 h-6 absolute top-[-12px] right-[-10px] flex justify-center items-center text-white rounded-full bg-blue-900 cursor-pointer">X</span>
+            <h2 className="font-capriola text-[30px] text-blue-900 text-center">TEAM <p className="text-green-600 inline">MEMBER</p></h2>
+            <div className='flex flex-col'>
+              1) Mohamed Ahmed Gameel
+              2) Ahmed Yasser Mahmoud
+              3) Karim Mohamed 
+              4) Ahmed Mohamed Ibrahem
+              5) Omar Wael
+            </div>
+          </div>
+        </div>
+      }
     </div>
     </div>
   )
